575ea75e09c478da0e36491b89e72179eeb7893f
Julie Nuguet <julie@nuguet.fr>
2013-09-04 21:03:04 +0200

    model

---
+++ src/model/Aeroport.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Aeroport {
+	private String codeAeroport; // code AITA
+	private String ville;
+	private String pays;
+	
+	public Aeroport(String codeAeroport, String ville, String pays) {
+		super();
+		this.codeAeroport = codeAeroport;
+		this.ville = ville;
+		this.pays = pays;
+	}
+
+	public String getCodeAeroport() {
+		return codeAeroport;
+	}
+
+	public void setCodeAeroport(String codeAeroport) {
+		this.codeAeroport = codeAeroport;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+}
--- src/model/Vol.java
+++ src/model/Vol.java
@@ -2,15 +2,10 @@ package model;
 
 import java.util.Date;
 
-// TODO : séparer "1 Vol possède une Destination"
 public class Vol {
 	private String id; // ex : DF1
-	private String villeDepart;
-	private String codeAeroportDepart;
-	private String paysDepart;
-	private String villeArrivee;
-	private String codeAeroportArrivee;
-	private String paysArrivee;
+	private Aeroport aeroportDepart;
+	private Aeroport aeroportArrivee;
 	private Date dateHeureDepart;
 	private int duree; // en minutes
 	private int tarif;
@@ -20,5 +15,122 @@ public class Vol {
 	private String codeHotesseSt2;
 	private String codeHotesseSt3;
 	
-	// à compléter
+	// un constructeur complet
+	public Vol(String id, Aeroport aeroportDepart, Aeroport aeroportArrivee,
+			Date dateHeureDepart, int duree, int tarif, String codePilote,
+			String codeCopilote, String codeHotesseSt1, String codeHotesseSt2,
+			String codeHotesseSt3) {
+		super();
+		this.id = id;
+		this.aeroportDepart = aeroportDepart;
+		this.aeroportArrivee = aeroportArrivee;
+		this.dateHeureDepart = dateHeureDepart;
+		this.duree = duree;
+		this.tarif = tarif;
+		this.codePilote = codePilote;
+		this.codeCopilote = codeCopilote;
+		this.codeHotesseSt1 = codeHotesseSt1;
+		this.codeHotesseSt2 = codeHotesseSt2;
+		this.codeHotesseSt3 = codeHotesseSt3;
+	}
+
+	// un constructeur sans les codes "employés"
+	public Vol(String id, Aeroport aeroportDepart, Aeroport aeroportArrivee,
+			Date dateHeureDepart, int duree, int tarif) {
+		super();
+		this.id = id;
+		this.aeroportDepart = aeroportDepart;
+		this.aeroportArrivee = aeroportArrivee;
+		this.dateHeureDepart = dateHeureDepart;
+		this.duree = duree;
+		this.tarif = tarif;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Aeroport getAeroportDepart() {
+		return aeroportDepart;
+	}
+
+	public void setAeroportDepart(Aeroport aeroportDepart) {
+		this.aeroportDepart = aeroportDepart;
+	}
+
+	public Aeroport getAeroportArrivee() {
+		return aeroportArrivee;
+	}
+
+	public void setAeroportArrivee(Aeroport aeroportArrivee) {
+		this.aeroportArrivee = aeroportArrivee;
+	}
+
+	public Date getDateHeureDepart() {
+		return dateHeureDepart;
+	}
+
+	public void setDateHeureDepart(Date dateHeureDepart) {
+		this.dateHeureDepart = dateHeureDepart;
+	}
+
+	public int getDuree() {
+		return duree;
+	}
+
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+
+	public int getTarif() {
+		return tarif;
+	}
+
+	public void setTarif(int tarif) {
+		this.tarif = tarif;
+	}
+
+	public String getCodePilote() {
+		return codePilote;
+	}
+
+	public void setCodePilote(String codePilote) {
+		this.codePilote = codePilote;
+	}
+
+	public String getCodeCopilote() {
+		return codeCopilote;
+	}
+
+	public void setCodeCopilote(String codeCopilote) {
+		this.codeCopilote = codeCopilote;
+	}
+
+	public String getCodeHotesseSt1() {
+		return codeHotesseSt1;
+	}
+
+	public void setCodeHotesseSt1(String codeHotesseSt1) {
+		this.codeHotesseSt1 = codeHotesseSt1;
+	}
+
+	public String getCodeHotesseSt2() {
+		return codeHotesseSt2;
+	}
+
+	public void setCodeHotesseSt2(String codeHotesseSt2) {
+		this.codeHotesseSt2 = codeHotesseSt2;
+	}
+
+	public String getCodeHotesseSt3() {
+		return codeHotesseSt3;
+	}
+
+	public void setCodeHotesseSt3(String codeHotesseSt3) {
+		this.codeHotesseSt3 = codeHotesseSt3;
+	}	
 }
