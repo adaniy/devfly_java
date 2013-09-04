b427e5099c6b4e1064e5978f17291574ac32ab96
Julie Nuguet <julie@nuguet.fr>
2013-09-04 10:38:37 +0200

    Test sous Windows concluant :)

--- src/ui/FenetrePrincipale.java
+++ src/ui/FenetrePrincipale.java
@@ -23,7 +23,6 @@ public class FenetrePrincipale extends JFrame {
 	private JLabel lblLogoGauche;
 	private JLabel lblLogoDroite;
 
-	// ligne de test à supprimer : versionnement du projet
 	/**
 	 * Launch the application.
 	 */
